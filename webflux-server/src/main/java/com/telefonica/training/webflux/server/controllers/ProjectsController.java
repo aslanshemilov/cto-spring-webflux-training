75c00a2acc7b02fd6ca7c7bcffe0e1ca744ea011
jorgelg <jorgelg@tid.es>
2019-05-14 16:52:04 +0200

    feat: full projects api

--- webflux-server/src/main/java/com/telefonica/training/webflux/server/controllers/ProjectsController.java
+++ webflux-server/src/main/java/com/telefonica/training/webflux/server/controllers/ProjectsController.java
@@ -6,12 +6,15 @@ package com.telefonica.training.webflux.server.controllers;
 
 import java.net.URI;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +23,7 @@ import org.springframework.web.util.UriTemplate;
 import com.telefonica.training.webflux.server.domain.Project;
 import com.telefonica.training.webflux.server.services.ProjectsService;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -42,9 +46,24 @@ public class ProjectsController {
 				});
 	}
 
-	@GetMapping("{projectId}")
+	@GetMapping
+	public Flux<Project> findProjects() {
+		return projectsService.findProjects();
+	}
+
+	@GetMapping("/{projectId}")
 	public Mono<Project> getProject(@PathVariable long projectId) {
 		return projectsService.getProject(projectId);
 	}
 
+	@PutMapping("/{projectId}")
+	public Mono<Project> updateProject(@RequestBody Project project) {
+		return projectsService.updateProject(project);
+	}
+
+	@DeleteMapping("/{projectId}")
+	public Mono<ResponseEntity<Void>> deleteProject(@PathVariable long projectId) {
+		return projectsService.deleteProject(projectId)
+				.thenReturn(new ResponseEntity<Void>(HttpStatus.NO_CONTENT));
+	}
 }
