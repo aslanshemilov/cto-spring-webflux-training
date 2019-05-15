5df3cface8f0c5d3455baffe569c89e9decfec17
jorgelg <jorgelg@tid.es>
2019-05-15 19:26:41 +0200

    feat: github client and repo report API

---
+++ webflux-server/src/main/java/com/telefonica/training/webflux/server/github/GithubClient.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) Telefonica I+D. All rights reserved.
+ */
+
+package com.telefonica.training.webflux.server.github;
+
+import java.util.Map;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.telefonica.training.webflux.server.domain.Project;
+import com.telefonica.training.webflux.server.domain.RepoReport;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class GithubClient {
+
+	private final WebClient webClient;
+
+	public GithubClient() {
+		webClient = WebClient.builder()
+				.baseUrl("https://api.github.com")
+				.defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+				.build();
+	}
+
+	public Mono<RepoReport> getRepoReport(Project project, String repo) {
+		return webClient.get()
+			.uri("/repos/{organization}/{repository}/languages", project.getOrganization(), repo)
+			.headers(headers -> {
+				if (project.getGithubToken() != null) {
+					headers.add(HttpHeaders.AUTHORIZATION, "token " + project.getGithubToken());
+				}
+			})
+			.retrieve()
+			.bodyToMono(Map.class)
+			.map(languages -> new RepoReport().setLanguages(languages));
+	}
+
+}
---
+++ webflux-server/src/main/java/com/telefonica/training/webflux/server/services/ReposService.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) Telefonica I+D. All rights reserved.
+ */
+
+package com.telefonica.training.webflux.server.services;
+
+import org.springframework.stereotype.Service;
+
+import com.telefonica.training.webflux.server.domain.RepoReport;
+import com.telefonica.training.webflux.server.exceptions.NotFoundException;
+import com.telefonica.training.webflux.server.github.GithubClient;
+
+import reactor.core.publisher.Mono;
+
+@Service
+public class ReposService {
+
+	private final ProjectsService projectsService;
+	private final GithubClient githubClient;
+
+	public ReposService(ProjectsService projectsService, GithubClient githubClient) {
+		this.projectsService = projectsService;
+		this.githubClient = githubClient;
+	}
+
+	public Mono<RepoReport> getRepoReport(long projectId, String repo) {
+		return projectsService.getProject(projectId)
+				.map(project -> {
+					if (project.getRepositories() == null || !project.getRepositories().contains(repo)) {
+						throw new NotFoundException();
+					}
+					return project;
+				})
+				.flatMap(project -> githubClient.getRepoReport(project, repo));
+	}
+
+}
